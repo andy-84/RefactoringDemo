68fff147dd0f5d20679f2fca5fcb01b59e758530
andy-84 <samaple>
2015-11-19 04:41:04 +0300

    added dependencies and a sample test

---
+++ src/test/java/RentalTest.java
@@ -0,0 +1,29 @@
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
+import com.scrumtrek.simplestore.Rental;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 19.11.2015.
+ */
+public class RentalTest {
+    @Test
+    public void ShouldReturnValidRentalDays() {
+        Movie dummyMovie = new Movie("dummyMovie", PriceCodes.Regular);
+        int daysRented = 3;
+        Rental testee = new Rental(dummyMovie, daysRented);
+
+        assertEquals(daysRented, testee.getDaysRented());
+    }
+
+    @Test
+    public void ShouldReturnValidMovie() {
+        Movie movie = new Movie("TestMovie", PriceCodes.Regular);
+        int dummyDaysRented = 3;
+        Rental testee = new Rental(movie, dummyDaysRented);
+
+        assertEquals(movie, testee.getMovie());
+    }
+}
+
